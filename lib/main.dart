e19e66ae47dec73d8849a2a003a4e62b8f16c10f
Dev4281 <divyansh.sharma0210@gmail.com>
2022-07-11 17:21:25 +0530

    chp1

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Awesome App",
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awsome App"),
+      ),
+      body: Container(
+        child: Center(child: Text("Hi Flutter")),
+      ),
+    );
+  }
+}
